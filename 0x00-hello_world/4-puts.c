d489a722913a58537717ef783590eb81798ae90b
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-17 03:57:12 -0800

    update p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-	char mystring[100] ="\"Programming is like a multilingual puzzle";
-	puts(mystring);
-	return(0);
+int main(void)
+{
+	char mystring[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts (mystring);
+	return (0);
 }
