52cac06c15cd67bd6ea9639d8a5ac06004d407e4
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-17 15:39:13 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints digits
+ * no arguments
+ * Description: prints all singles 
+ * digits of base ten
+ * folowed by new line
+ * Return: zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d",a);
+	}
+	printf("\n");
+	return (0);
+}
