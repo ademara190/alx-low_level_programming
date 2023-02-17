7f496b0be2fa8b260a28c1af97da7c9ac1647705
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-17 04:58:10 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function prints strings using printf
+ * function has no arguments
+ * Description : This function initailizes a string
+ * which contains the string to be printed
+ * it then prints it using printf
+ * Return : returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
