6a04bc30199332275aa3d1b38d6942806864210d
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-17 04:01:13 -0800

    update p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+ * This function uses puts to print string
+ */
 int main(void)
 {
 	char mystring[100] = "\"Programming is like building a multilingual puzzle";
 
-	puts (mystring);
+	puts(mystring);
 	return (0);
 }
