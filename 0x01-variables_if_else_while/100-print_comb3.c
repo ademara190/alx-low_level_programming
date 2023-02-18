4f98479853bf3b94f920845a26fb15886541d1bc
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-17 18:08:21 -0800

    printcombo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints possible combination of two digits
+ * no arguments
+ * Description: prints possible combination
+ * of two digits
+ * Return: zero
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			if (num1 != num2 && num2 > num1)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
