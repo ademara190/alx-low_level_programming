d98a102575bdc49397b7e3953672c8a453984a50
oluwatoyin <ademaratioluwatoyin20@gmail.com>
2023-02-21 05:47:07 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function prints _purchar
+ * no arguments
+ * Description: This function prints
+ * putchar followed by new line
+ * Return: zero
+ */
+int main()
+{
+	printf("_purchar\n");
+	return (0);
+}
